415dc6b61e22991c9c069dda72ec11cede5f92e5
20865991 <20865991@student.curtin.edu.au>
2022-05-31 16:55:08 +0800

    new test implementation

---
+++ CODE/conversion1Test.java
@@ -0,0 +1,8 @@
+public class conversion1Test 
+{
+    public static void main(String[] args)
+    { 
+        assert "222.779".equals(lengthConverter.conversion2("file.txt, 67.9")): "file contains data in metres";
+        assert "30.266".equals(lengthConverter.conversion2("file.txt, 99.3")): "file contains data in ft";
+    }
+} 
---
+++ CODE/conversion2Test.java
@@ -0,0 +1,9 @@
+public class conversion2Test 
+{
+    public static void main(String[] args)
+    { 
+        assert "177.546".equals(lengthConverter.conversion2("file.txt, 69.9")): "file contains data in inches";
+        assert "17.125".equals(lengthConverter.conversion2("file.txt, 43.5")): "file contains data in cm";
+        
+    }
+}
